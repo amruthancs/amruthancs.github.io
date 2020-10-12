5ee273ec69723b8e51938613a2366855acff2a14
amruthancs <45876883+amruthancs@users.noreply.github.com>
2020-10-12 20:59:03 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -9,7 +9,7 @@ const settings = {
 const greeting = {
   title: "Amruthan C S",
   logo_name: "Amruthan C S",
-  nickname: "Mechon",
+  nickname: "Adorable Mechon",
   subTitle:
     "A passionate individual who always thrive to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
@@ -41,31 +41,31 @@ const socialMediaLinks = [
   },
   {
     name: "YouTube",
-    link: "https://www.youtube.com/",
+    link: "https://www.youtube.com/channel/UCtOmaY76PaXygB_DuUCdnOg",
     fontAwesomeIcon: "fa-youtube", // Reference https://fontawesome.com/icons/youtube?style=brands
     backgroundColor: "#FF0000", // Reference https://simpleicons.org/?q=youtube
   },
   {
     name: "Gmail",
-    link: "mailto:ashutoshhathidara98@gmail.com",
+    link: "mailto:amtn2000@gmail.com",
     fontAwesomeIcon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
     backgroundColor: "#D14836", // Reference https://simpleicons.org/?q=gmail
   },
   {
     name: "Twitter",
-    link: "https://twitter.com/ashutosh_1919",
+    link: "https://twitter.com/amruthan2000",
     fontAwesomeIcon: "fa-twitter", // Reference https://fontawesome.com/icons/twitter?style=brands
     backgroundColor: "#1DA1F2", // Reference https://simpleicons.org/?q=twitter
   },
   {
     name: "Facebook",
-    link: "https://www.facebook.com/laymanbrother.19/",
+    link: "https://www.facebook.com/amruthan.venkatesh.7/",
     fontAwesomeIcon: "fa-facebook-f", // Reference https://fontawesome.com/icons/facebook-f?style=brands
     backgroundColor: "#1877F2", // Reference https://simpleicons.org/?q=facebook
   },
   {
     name: "Instagram",
-    link: "https://www.instagram.com/layman_brother/",
+    link: "https://www.instagram.com/amruthan_venkatesh/",
     fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
     backgroundColor: "#E4405F", // Reference https://simpleicons.org/?q=instagram
   },
@@ -73,7 +73,7 @@ const socialMediaLinks = [
 
 const skills = {
   data: [
-    {
+   /* {
       title: "Data Science & AI",
       fileName: "DataScienceImg",
       skills: [
@@ -304,7 +304,7 @@ const skills = {
       ],
     },
   ],
-};
+};*/
 
 // Education Page
 const competitiveSites = {
